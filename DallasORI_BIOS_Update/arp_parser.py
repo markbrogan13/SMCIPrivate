202fb5d0cd4dc92bf125af21c10e9e72a99ec1f1
Mark Brogan <mbrogan@MARKMBP14M2.local>
2024-07-11 13:37:24 -0700

    added modulatrity with subnet start/end scope

--- DallasORI_BIOS_Update/arp_parser.py
+++ DallasORI_BIOS_Update/arp_parser.py
@@ -21,16 +21,25 @@ from tqdm import tqdm
 import platform
 import subprocess
 
-ARP_FILE_NAME = "/Users/mbrogan/Downloads/DallasORI/Parser/arp_out.txt"
-MAC_BMC_PASS = "/Users/mbrogan/Downloads/DallasORI/Parser/mac_pass.txt"
+ARP_FILE_NAME = "/Users/mbrogan/Downloads/DallasORI/Parser/DallasORI_BIOS_Update/arp_out.txt"
+MAC_BMC_PASS = "/Users/mbrogan/Downloads/DallasORI/Parser/DallasORI_BIOS_Update/mac_pass.txt"
 ARP_DICTIONARY = []
 MAC_DICTIONARY = []
 
-def ping_all(subnet):
-    iterations = 255
-    ping_result = subprocess.Popen(['./ping_all.sh', subnet], stdout=subprocess.PIPE, text=True)
+
+"""
+    invoke a shell script to ping all hosts on the 192.168.8.0/24 subnet
+    -> can be changed within the def call
+
+    with a terminal open, will provide a progress bar for the process
+"""
+def ping_all(subnet, start, finish):
+    iterations = finish - start + 1
+    ping_result = subprocess.Popen(['./ping_all.sh', subnet, str(start), str(finish)], stdout=subprocess.PIPE, text=True) # starts a readable invokation
     
-    progress_bar = tqdm(total=iterations)
+    progress_bar = tqdm(total=iterations) # progress bar
+
+    # progress ar iterator
     while True:
         ping_output = ping_result.stdout.readline()
         if ping_output == '' and ping_result.poll() is not None:
@@ -39,15 +48,24 @@ def ping_all(subnet):
             progress_bar.update(1)
     progress_bar.close()
 
+
+"""
+    calls 'arp -an' for the current device's arp table to get the MACs on subnet
+    this will write all arp cached entries to file after removing the previous file
+"""
 def arp_out():
-    os.remove("arp_out.txt")
+    os.remove("arp_out.txt") # remove old data
     with open("arp_out.txt", "w") as arp_file:
         command = "arp -an"
-        arp_an = subprocess.run(command, shell=True, capture_output=True, text=True)
+        arp_an = subprocess.run(command, shell=True, capture_output=True, text=True) # runs the command 'arp -an'
         arp_file.write(arp_an.stdout)
         
         arp_file.close()
 
+
+"""
+    read through the arp_out.txt file and calls a helper function to parse the output
+"""
 def read_arp():
     with open(ARP_FILE_NAME, "r") as arps:
         for line in arps.readlines():
@@ -58,6 +76,11 @@ def read_arp():
         
         arps.close()
 
+
+"""
+    regex to strip down and format the output to return MAC address and OOB IP
+    TODO: Fix the last hextet to stop dropping the leading 0 (ex :07, gets added as '7')
+"""
 def regExHelper(arp_arr):
     text = arp_arr[0].strip()
     match = re.search(r'\((.*?)\)', text)
@@ -83,6 +106,12 @@ def regExHelper(arp_arr):
     else:
         return [mac_address, oob_ip]
 
+
+"""
+    take the MAC addresses and cross reference with the mac_pass.txt to get unique password to add:
+    'OOB_IP ADMIN BMC_UNIQUE_PASSWD'
+    to the input_file.txt
+"""
 def match_mac_to_input():
     with open(MAC_BMC_PASS, "r") as mac_pass:
         for line in mac_pass.readlines():
@@ -103,7 +132,10 @@ def match_mac_to_input():
         
         input_file.close()
 
-
+"""
+    invokes the BIOS updater script and takes in the input_file
+    script runs through all devices in the file and reports failures/successes
+"""
 def update_BIOS(input_file):
     line_count = 0
     with open(input_file, 'r') as file:
@@ -128,8 +160,16 @@ if __name__ == "__main__":
         print(f"OS Name: {os.name}\n" +
               f"Operating System: {platform.system()}\n" +
               f"Version: {platform.version()}\n")  
-        ping_all(subnet="192.168.8")
+        
+        ping_all(subnet="192.168.8", start=1, finish=255)
         arp_out()
         read_arp()
         match_mac_to_input()
         update_BIOS(input_file="input_file.txt")
+
+"""
+    Possible improvements: 
+    TODO: adding a GUI or Windows support via batch scripts vs shell
+    TODO: more modularity in a subset of BIOS changes that you can have the script change
+    TODO: more modularity with subnet scope
+"""
--- DallasORI_BIOS_Update/ping_all.sh
+++ DallasORI_BIOS_Update/ping_all.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 subnet=$1
+start=${2:-1}
+finish=${3:-255}
 
-for i in {1..255}; do
+for i in $(seq $start $finish); do
   ip="$subnet.$i"
   ping -c 1 -W 1 $ip > /dev/null 2>&1
   if [ $? -eq 0 ]; then
